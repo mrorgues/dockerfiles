c2a2e772f0783d3ebb77ff43a3294fb84fce25a9
mrorgues <mrorgues@users.noreply.github.com>
2018-04-02 16:07:32 +0200

    worldCamera.js: Fix for azerty keyboards

--- opensc2k/config/change_controls_for_azerty_keyboards.sh
+++ opensc2k/config/change_controls_for_azerty_keyboards.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+set -e
 
+if [ $KEYBOARD_TYPE = "AZERTY" ]
+then
+    echo "Fixing controls for $KEYBOARD_TYPE keyboards."
+    sed -i "s,'up': Phaser.Input.Keyboard.KeyCodes.W,'up': Phaser.Input.Keyboard.KeyCodes.Z,g" /root/OpenSC2K/src/world/worldCamera.js
+    sed -i "s,'left': Phaser.Input.Keyboard.KeyCodes.A,'left': Phaser.Input.Keyboard.KeyCodes.Q,g" /root/OpenSC2K/src/world/worldCamera.js
+    sed -i "s,'zoomIn': Phaser.Input.Keyboard.KeyCodes.Q,'zoomIn': Phaser.Input.Keyboard.KeyCodes.A,g" /root/OpenSC2K/src/world/worldCamera.js
+    sed -i "s,this.scene.input.keyboard.on('keydown_Z',this.scene.input.keyboard.on('keydown_W',g" /root/OpenSC2K/src/world/worldCamera.js
+    echo "Done!"
+fi
